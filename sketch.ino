da587d8b148513a92bed591cb46ca02706d36561
AleAraGo <163934517+AleAraGo@users.noreply.github.com>
2024-05-24 21:14:21 -0300

    Initial commit

---
+++ sketch.ino
@@ -0,0 +1,31 @@
+#define leds 4
+#define butao 15
+int roxinho[leds]= {2,0,16,5};
+int rozinha[leds]= {19,21,22,23};
+
+void setup() {
+  for(int i=0;i<leds;i++){
+    pinMode(roxinho[i], OUTPUT);
+    pinMode(rozinha[i], OUTPUT);
+  }
+pinMode(butao, INPUT_PULLUP);
+}
+
+void loop() {
+  while(digitalRead(butao) == HIGH){
+    for(int i= 0; i<leds;i++){
+      digitalWrite(roxinho[i], HIGH);
+      digitalWrite(rozinha[leds -1-i],HIGH);
+      delay(250);
+      digitalWrite(roxinho[i], LOW);
+      digitalWrite(rozinha[leds -1-i],LOW);  
+    }    
+  }
+      for(int i= 0; i<leds;i++){
+      digitalWrite(roxinho[i],HIGH);
+      digitalWrite(rozinha[i],HIGH);
+      delay(250);
+      digitalWrite(roxinho[i],LOW);
+      digitalWrite(rozinha[i],LOW);  
+    }
+}
